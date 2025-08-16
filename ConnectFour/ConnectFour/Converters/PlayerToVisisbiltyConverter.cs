3927b04b559a2bfa96a4d42634c65b2937fcc2bf
ahmedeceng87 <ahmedeceng87@gmail.com>
2025-08-16 22:15:39 +0530

    feat: fixed UI issue where the container hole was becoming blue for small duration

---
+++ ConnectFour/ConnectFour/Converters/PlayerToVisisbiltyConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace ConnectFour.Converters
+{
+    public class PlayerToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Player player && player != Player.None)
+                return Visibility.Visible;
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
